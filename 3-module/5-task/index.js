4aafe7234336ece83e426929300de6715e760c93
PavelK <overhandz@gmail.com>
2021-05-30 09:45:24 +0300

    функция которая возвращает обхъект с минимальными и максимльным сислом из строки

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,21 @@
 function getMinMax(str) {
-  // ваш код...
+  whoa = [...str]
+  .map(i => {if(isFinite(i) == true || i == "." || i == "-"){
+    return i
+  } else {
+    return " "}
+  })
+  .join("")
+  .split(" ")
+  .filter(i => i != "")
+  .map(i => Number(i));
+  let min = Math.min(...whoa);
+  let max = Math.max(...whoa);
+  let finalObj = {
+    min: min ,
+    max: max
+  };
+  return (finalObj);
 }
+
+console.log (getMinMax('1, -5.8 или 10, хотя 34 + -5.3 и 73'));
