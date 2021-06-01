7ec0fa65631f4cfd26103bd87217df6220bb34fe
PavelK <overhandz@gmail.com>
2021-06-01 22:30:32 +0300

    карусель изображений

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  let slideInner = document.querySelector ('.carousel__inner');
+  let slideRightArrow = document.querySelector ('.carousel__arrow_right');
+  let slideLeftArrow = document.querySelector ('.carousel__arrow_left');
+  let slides = document.querySelectorAll ('.carousel__slide');
+  let slideWidth = slideInner.offsetWidth;
+  let slidePosition = 1;
+
+slideLeftArrow.style.display ='none';
+
+slideRightArrow.addEventListener ('click', () => {
+  slideInner.style.transform = `translateX(-${slideWidth * slidePosition++ + 'px'})`;
+    if (slidePosition > slides.length -1) {
+      slideRightArrow.style.display = 'none';
+    } else {
+      slideRightArrow.style.display = '';
+    }
+    if (slidePosition > 1) {
+      slideLeftArrow.style.display = '';
+    }
+});
+
+slideLeftArrow.addEventListener ('click', () => {
+  slideInner.style.transform = `translateX(-${slideWidth * (--slidePosition -1) + 'px'})`;
+  if (slidePosition > slides.length -1) {
+    slideRightArrow.style.display = 'none';
+  } else {
+    slideRightArrow.style.display = '';
+  }
+  if (slidePosition == 1) {
+    slideLeftArrow.style.display = 'none';
+  }
+});
 }
