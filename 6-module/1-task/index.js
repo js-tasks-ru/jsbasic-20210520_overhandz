b63fe35d817f746b5f9444d91c27a66bcaf5e8f3
PavelK <overhandz@gmail.com>
2021-06-07 19:38:42 +0300

    Самоудаляющаяся таблица

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,24 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
-  constructor(rows) {
+constructor(rows) {
+let newTable = document.createElement ('table');
+let tbody = document.createElement ('tbody');
+rows.forEach (item => {
+let newRow = document.createElement('tr');
+newRow.innerHTML = `
+<td>${item.name}</td>
+<td>${item.age}</td>
+<td>${item.salary}</td>
+<td>${item.city}</td>
+<td><button>X</button></td>
+`;
+newTable.append(tbody);
+tbody.append (newRow);
+});
+newTable.addEventListener ('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    e.target.parentElement.parentElement.remove (); 
   }
+});
+this.elem = newTable;
+}
 }
